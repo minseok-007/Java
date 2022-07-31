e94a3c1804341ee9ef3fd4119940b5c24ce9c40a
mc1920 <mc1920@Ic.ac.uk>
2022-07-31 16:19:34 +0900

    Initial commit

---
+++ src/operator.java
@@ -0,0 +1,31 @@
+public class operator {
+    public static void main(String[] args) {
+        // 1 + 2 (+: operator; +, -, *, /; 1, 2: operand)
+        // <, >, <=, >=, ==, !=
+        // && || ! ...
+        // =
+
+        int a = 10;
+        int b = 4;
+        int c = a % b;
+
+        System.out.println(a != b);
+
+        // go to school    ||     go to restaurant
+        //      true                true            true
+
+        // true true
+        // true false
+        // false true
+        // false false
+
+        // i % 2 == 0   &&   i % 3 == 0
+
+        //6 12 18 24
+
+        2 4 6 8 10 12 14 16 18
+            3 6 9 12 15 18
+
+
+    }
+}
---
+++ src/variable.java
@@ -0,0 +1,21 @@
+public class variable {
+    public static void main(String[] args) {
+
+        // 변수타입 변수이름
+        int age = 15; // 변수의 초기화
+        System.out.println(age);
+
+        age = age + 1;
+
+        System.out.println(age);
+
+        // double: 1.0, 2/3, root(3)
+        // char: 'A'
+        // long
+
+        char c = 'A';
+        System.out.println(c);
+
+
+    }
+}
